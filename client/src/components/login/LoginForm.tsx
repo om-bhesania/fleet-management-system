0b10f0fe835c7eb61fd5cb510665f2ac180f3900
Ombhesania <bhesaniaom@gmail.com>
2024-08-05 23:47:30 +0530

    frontend initial commit

---
+++ client/src/components/login/LoginForm.tsx
@@ -0,0 +1,10 @@
+
+const LoginForm = () => {
+    return (
+        <div>
+            <div className="text-5xl text-red-500">This is LoginForm.js</div>
+        </div>
+    );
+}
+
+export default LoginForm;
