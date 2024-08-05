aa565092b37a7374976eaabde6c812932dc3db9c
Ombhesania <bhesaniaom@gmail.com>
2024-08-05 23:34:01 +0530

    initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="text-5xl text-red-500">This is App.js</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container:{
+        center: true,
+        padding: "1rem",
+        screens:{
+          sm: "100%",
+          md: "100%",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+        }
+      }
+    },
+  },
+  plugins: [],
+};
