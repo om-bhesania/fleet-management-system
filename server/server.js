3c65b07ce4a7840abd87b83000d89e4e4a988756
Ved Panchal <vedpanchal99@gmail.com>
2024-08-08 22:57:29 +0530

    Added Cluster

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 import dotenv from 'dotenv';
+import cluster from "node:cluster";
+import os from "node:os";
 import app from './app.js';
 
 dotenv.config();
 
+// const numCPUs = os.cpus().length;
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if(cluster.isPrimary){
+    for(let i=0;i<2;i++){
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+        cluster.fork();
+    });
+} else {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT} (Worker ${process.pid})`);
+    }); 
+}
